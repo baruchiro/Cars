0203534073a9f678754eb47fcf20fe6b8128ec90
Baruch Rothkoff <baruchiro@gmail.com>
2016-11-03 14:15:06 +0200

    MyAccountFragment show only next rents And design

--- app/src/main/java/rothkoff/baruch/cars/Customer.java
+++ app/src/main/java/rothkoff/baruch/cars/Customer.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:18100a9733f2bb7cec034fc87cc70a7f1d6af0919d94b6fb37c108b8a09af054
-size 3339
+oid sha256:b496f207250d1270937910f453af64a9844fd7f5eb3ceb3ae01049183c4f187c
+size 3679
--- app/src/main/java/rothkoff/baruch/cars/MyAccountFragment.java
+++ app/src/main/java/rothkoff/baruch/cars/MyAccountFragment.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:22847e479acc6c07c0f8954af85b66f5c84fe3ffc3d73946ef020026f9781293
-size 2951
+oid sha256:5a2e5216abe89c270858e3cca79755b98819e1af9d0073cc5a2141d1cf807e02
+size 2958
