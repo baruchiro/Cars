76ccffc8b422c27ae537fdabd3cf5470089e8c1a
Baruch Rothkoff <baruchiro@gmail.com>
2016-11-22 13:00:34 +0200

    success show photo

--- app/src/main/java/rothkoff/baruch/cars/CarActivity.java
+++ app/src/main/java/rothkoff/baruch/cars/CarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a84644468c513e20915b87ba4f78e5932df1f0c1ee2145ab417aaa54df18ef52
-size 4317
+oid sha256:7f2eab9f4b30a58c2ffeea7eb28bc76bf64bd48a1ab5a7b407fd1d0a6a7c0561
+size 4109
