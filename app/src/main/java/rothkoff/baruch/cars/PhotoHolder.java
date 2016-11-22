59bb3839e10d469094097c52954e1b5c89ed745c
Baruch Rothkoff <baruchiro@gmail.com>
2016-11-22 12:52:13 +0200

    success show photo

--- app/src/main/java/rothkoff/baruch/cars/PhotoHolder.java
+++ app/src/main/java/rothkoff/baruch/cars/PhotoHolder.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:1ea1a72d8e86ac9e7e94ea6531ea398980270e46ec3cc0dcbd63772ef2543169
-size 3221
+oid sha256:15583bc7060ca8025179858ca4bdb734c3602ab773216f4ba3d116035b7497dc
+size 3087
