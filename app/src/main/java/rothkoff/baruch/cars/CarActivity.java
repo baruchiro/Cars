823d4d07afd2617eec206f636d560095be2b0dac
Baruch Rothkoff <baruchiro@gmail.com>
2016-11-27 10:17:38 +0200

    Upload PDF to Firebase and Activate Rent

--- app/src/main/java/rothkoff/baruch/cars/CarActivity.java
+++ app/src/main/java/rothkoff/baruch/cars/CarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6859cdd188500c80499d7d674a7550061a573842cecd43111bbc81186f9962bf
-size 21149
+oid sha256:4d71d3d733bea0cd94060d5d74f1edbf97335dd5d913b1176e02d7a5dffe3141
+size 21272
