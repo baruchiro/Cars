6e1559ba3d891099bef6442a4957472568dd7e8c
Baruch Rothkoff <baruchiro@gmail.com>
2016-11-30 20:24:01 +0200

    Add MenuItem for Update END

--- app/src/main/java/rothkoff/baruch/cars/MainActivity.java
+++ app/src/main/java/rothkoff/baruch/cars/MainActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:85d3265cc7a16bd68a2dd57f8d2121bc2beb833dedc215551ffd7cdb5925debe
-size 12634
+oid sha256:1b6b1615e8c14a4ed133dac033823710a2b2b01b86c28db042bbc2b6aebb8dd6
+size 13241
