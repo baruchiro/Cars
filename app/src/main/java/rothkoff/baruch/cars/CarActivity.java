dd5e163fcfbac88ef9a6eb487eb22d60991b954b
Baruch Rothkoff <baruchiro@gmail.com>
2016-11-30 22:15:43 +0200

    Take some pictures in one action add button to update V313

--- app/src/main/java/rothkoff/baruch/cars/CarActivity.java
+++ app/src/main/java/rothkoff/baruch/cars/CarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:052dfbdca189c3ec8411b4392bef80bf8a37eede65cb0e86e47bfc6d878e0b04
-size 22303
+oid sha256:9cb8b59ff9ea331d2b09546add4f96e85f0c369c30b698d53b2fcfc6da17ae8a
+size 25370
