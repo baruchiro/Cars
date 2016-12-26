6077e32236d92e5ee87b0ac2b4a21178be393473
Baruch Rothkoff <baruchiro@gmail.com>
2016-12-26 12:06:05 +0200

    26/12

--- app/src/main/java/rothkoff/baruch/cars/CarActivity.java
+++ app/src/main/java/rothkoff/baruch/cars/CarActivity.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:72c3c68e8a2e124a25e9e5aeacf04123f76d1c84766b10a9bbe1318e5734a24b
-size 25184
+oid sha256:52a577d3f7e30775cff643369519ad343940bd26d1879ce4221a9a74a388fb49
+size 25245
--- app/src/main/java/rothkoff/baruch/cars/PhotoHolder.java
+++ app/src/main/java/rothkoff/baruch/cars/PhotoHolder.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d73da2621e7587c687caf44138c217c9a234d520827a89de1348082de86e5c31
-size 3373
+oid sha256:69baf3ba29bd0fcae2d932199836dc6db1100c93e5109478f17705ee20e9ad03
+size 3558
