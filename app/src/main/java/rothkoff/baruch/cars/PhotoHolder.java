cb561741be2ad7549cbe61d1be08f323be7c0cfd
Baruch Rothkoff <baruchiro@gmail.com>
2016-12-27 12:47:24 +0200

    V320 Take picture, Upload and Show

--- app/src/main/java/rothkoff/baruch/cars/PhotoHolder.java
+++ app/src/main/java/rothkoff/baruch/cars/PhotoHolder.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:69baf3ba29bd0fcae2d932199836dc6db1100c93e5109478f17705ee20e9ad03
-size 3558
+oid sha256:7d8b4f1dcbf73298e580a278fd19df5ab4e77f5271a05bedc4d9dc49e0f34f52
+size 3489
